23ac7a1287d8903117ce8787347b540bf63caaa0
nehalskdev <nikesk1999@gmail.com>
2026-02-04 15:50:39 +0530

    practiced not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+function Notfound() {
+  return (
+    <div>
+      <h1>page Not found</h1>
+      <p>Could not find requested resource.</p>
+    </div>
+  );
+}
+
+export default Notfound;
--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,14 @@
+import { Notfound } from "next/navigation";
+
 async function ProductReview({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
   const { productId, reviewId } = await params;
+  if (parseInt(reviewId > 1000)) {
+    Notfound();
+  }
   return (
     <div>
       Review {reviewId} and product {productId}
