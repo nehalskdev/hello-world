ac6d66591c8fd27e996c262516d1f2407cffaf62
nehalskdev <nikesk1999@gmail.com>
2026-02-05 16:02:30 +0530

    learned adding loading effectively

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
+// import { resolve } from "path";
 
-function Blog() {
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function Blog() {
+  await delay(2000);
   return (
     <>
-      <Link href="/">Home</Link>
+      {/* <Link href="/">Home</Link> */}
       <div>Blog page</div>
     </>
   );
