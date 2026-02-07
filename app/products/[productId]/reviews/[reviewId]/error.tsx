e26dd6f89760f526e4c32e751d5204515842a3f5
nehalskdev <nikesk1999@gmail.com>
2026-02-07 12:49:34 +0530

    learned error handling

---
+++ app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+function ErrorBoundry() {
+  return <div>Error in review ID</div>;
+}
+
+export default ErrorBoundry;
--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,18 @@
 import { redirect } from "next/navigation";
 
+function getRandomInt(count: Number) {
+  return Math.floor(Math.random() * count);
+}
+
 async function ProductReview({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error("Error loading in review");
+  }
   const { productId, reviewId } = await params;
   if (Number(reviewId) > 1000) {
     redirect("/products");
